a6bc0d23944b5eb15937624539c13018b98de83a
songliangliang <18610090485@163.com>
2017-05-15 15:29:20 +0800

    add new method

--- src/liang/Liang.java
+++ src/liang/Liang.java
@@ -4,4 +4,8 @@ public class Liang {
 	public static void main(String[] args) {
 		System.out.println("liang");
 	}
+	private void mehtod1() {
+		// TODO Auto-generated method stub
+		System.out.println("mehtod1");
+	}
 }
