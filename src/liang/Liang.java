a123f82d232e0348139aa7a4c8c7fb8f4513c279
songliangliang <18610090485@163.com>
2017-05-15 15:42:33 +0800

    修改输出内容

--- src/liang/Liang.java
+++ src/liang/Liang.java
@@ -2,6 +2,10 @@ package liang;
 
 public class Liang {
 	public static void main(String[] args) {
-		System.out.println("liang");
+		System.out.println("xuanzi");
+	}
+	private void mehtod1() {
+		// TODO Auto-generated method stub
+		System.out.println("mehtod1");
 	}
 }
