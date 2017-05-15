041019c42e591cfc1f75ca2bb1d5b6b4c9523d42
songliangliang <18610090485@163.com>
2017-05-15 15:49:15 +0800

    1

--- src/liang/Liang.java
+++ src/liang/Liang.java
@@ -4,10 +4,7 @@ public class Liang {
 	public static void main(String[] args) {
 		System.out.println("xuanzi");
 	}
-	private void mehtod1() {
-		// TODO Auto-generated method stub
-		System.out.println("mehtod1");
-	}
+	
 	private void mehtod1() {
 		// TODO Auto-generated method stub
 		System.out.println("mehtod1");
